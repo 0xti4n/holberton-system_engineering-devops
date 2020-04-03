fb948d02749244348a9f48a0bbcc3ffb81bad08b
Cristian <1154@holbertonschool.com>
2020-04-03 22:01:17 +0000

    Adding task 2

--- 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
@@ -2,6 +2,6 @@
 # header with puppet
 
 exec { 'header':
-command  => 'sudo apt-get update && sudo apt-get install -y nginx && echo "Holberton School for the win!" | sudo tee /var/www/html/index.html && sed -i "/server_name _;/a rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;" /etc/nginx/sites-available/default && sudo sed -i "/http {/a \ \tadd_header X-Served-By $HOSTNAME;" /etc/nginx/nginx.conf && sudo service nginx restart'
+command  => 'sudo apt-get update && sudo apt-get install -y nginx && echo "Holberton School for the win!" | sudo tee /var/www/html/index.html && sed -i "/server_name _;/a rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;" /etc/nginx/sites-available/default && sudo sed -i "/http {/a \ \tadd_header X-Served-By $HOSTNAME;" /etc/nginx/nginx.conf && sudo service nginx restart',
 provider => shell,
 }
