9b5a49589cbc204a58caae3a839f62f9fc123578
Cristian <1154@holbertonschool.com>
2020-05-04 20:53:52 +0000

    Updating task 0

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """returns information about his/her todo list progress"""
-from sys import argv
 import requests
+from sys import argv
 
 if __name__ == "__main__":
     av = argv[1]
