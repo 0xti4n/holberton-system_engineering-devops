f0954b2206c39dd9fa9947350689a59fc1967558
Cristian <1154@holbertonschool.com>
2020-05-04 14:35:08 +0000

    Updating readme and add task 0

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""returns information about his/her todo list progress."""
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    av = argv[1]
+
+    user = 'https://jsonplaceholder.typicode.com/users/{}'.format(av)
+    r2 = 'https://jsonplaceholder.typicode.com/users/{}/todos'.format(av)
+
+    r_user = requests.get(user)
+    r2 = requests.get(r2)
+
+    data_user = r_user.json()
+    data_route2 = r2.json()
+
+    name = data_user['name']
+    con = 0
+    t_task = 0
+    t_done = []
+    for task in data_route2:
+        t_task += 1
+        if task['completed'] is True:
+            t_done.append(task['title'])
+            con += 1
+
+    print('Employee {} is done with tasks({}/{}):'.format(name, con, t_task))
+    for done in t_done:
+        print('\t{}'.format(done))
