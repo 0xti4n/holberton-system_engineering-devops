c92fd4c2a0b3b8edf0b11cd0eefab4f0594a00a2
Cristian <1154@holbertonschool.com>
2020-05-07 22:38:33 +0000

    Adding task 100

---
+++ 0x16-api_advanced/100-count.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""queries the Reddit API and returns a
+list containing the titles of all hot articles
+for a given subreddit. If no results are found for
+the given subreddit, the function should return None."""
+import requests
+import operator
+
+
+def count_words(subreddit, word_list, next_page='', new_ditc={}):
+    """queries recursively hot titles"""
+    url = 'https://www.reddit.com/r/{}/hot'.format(subreddit)
+    queri = url + '.json?&after={}'.format(next_page)
+    header = {'User-agent': 'agent xtian'}
+
+    r = requests.get(queri, headers=header, allow_redirects=False)
+    if r.status_code == 302:
+        return
+
+    else:
+        if len(new_ditc) == 0:
+            new_ditc = dict.fromkeys(word_list, 0)
+        concat = ''
+        titles = r.json()
+        for indx in titles['data']['children']:
+            concat += indx['data']['title']
+            all_titles = concat.split(' ')
+
+        for indx in all_titles:
+            for k, v in new_ditc.items():
+                if k == indx:
+                    new_ditc[k] += 1
+
+        next_page = titles['data']['after']
+        if next_page is None:
+            sort_values = sorted(new_ditc.items(),
+                                 key=operator.itemgetter(1),
+                                 reverse=True)
+            for i in sort_values:
+                print('{}: {}'.format(i[0], i[1]))
+        else:
+            return count_words(subreddit, word_list, next_page, new_ditc)
